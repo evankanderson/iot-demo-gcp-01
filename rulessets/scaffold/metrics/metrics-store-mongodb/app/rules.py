b98c6868b7a06bfc4b9bb8054e7bee21615f70dc
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-04-22 12:07:59 +0200

    add metics patch

--- rulessets/scaffold/metrics/metrics-store-mongodb/app/rules.py
+++ rulessets/scaffold/metrics/metrics-store-mongodb/app/rules.py
@@ -8,7 +8,7 @@ ruledata = Const.RULEDATA
 filters = Const.FILTERS
 processing = Const.PROCESSING
 
-from krules_core.providers import results_rx_factory
+from krules_core.providers import results_rx_factory, settings_factory
 from krules_env import publish_results_errors, publish_results_all, publish_results_filtered
 from krules_mongodb import WithDatabase, WithCollection, MongoDBInsertOne
 from dateutil.parser import parse
@@ -26,6 +26,8 @@ results_rx_factory().subscribe(
 )
 
 DBNAME = "kr-dev-03"
+mongodb_settings = subjects_redis_storage_settings = settings_factory() \
+        .get("scaffold").get("metrics").get("mongodb")
 
 rulesdata = [
 
@@ -37,8 +39,9 @@ rulesdata = [
         subscribe_to: TopicsDefault.RESULTS,
         ruledata: {
             processing: [
-                WithDatabase(DBNAME),
-                WithCollection("metrics", indexes=[IndexModel([("origin_id", HASHED)])], capped=True, size=1000000),
+                WithDatabase(mongodb_settings["database"]),
+                WithCollection(mongodb_settings["collection"], indexes=[IndexModel([("origin_id", HASHED)])],
+                               capped=True, size=1000000),
                 SetPayloadProperty("origin_id", lambda payload: payload["payload"]["_event_info"]["Originid"]),
                 SetPayloadProperty("time", lambda payload: parse(payload["payload"]["_event_info"]["Time"])),
                 MongoDBInsertOne(lambda payload: payload),
