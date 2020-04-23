3662ba632e06fe7126b6810c8cdc63bd1db401cd
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-04-23 11:02:13 +0200

    implement pubsub-source-subscription

--- rulessets/scaffold/ingestion/pubsub-source-subscription/app/rules.py
+++ rulessets/scaffold/ingestion/pubsub-source-subscription/app/rules.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from krules_core.base_functions import *
 
 from krules_core import RuleConst as Const
@@ -8,8 +10,11 @@ ruledata = Const.RULEDATA
 filters = Const.FILTERS
 processing = Const.PROCESSING
 
+
 from krules_core.providers import results_rx_factory
 from krules_env import publish_results_errors, publish_results_all, publish_results_filtered
+
+from dateutil.parser import parse
 
 # import pprint
 # results_rx_factory().subscribe(
@@ -22,18 +27,44 @@ results_rx_factory().subscribe(
     on_next=publish_results_errors,
 )
 
+
+class IsNotOutdated(RuleFunctionBase):
+
+    def execute(self, key_func):
+
+        key = key_func(self)
+        event_time = parse(self.payload["_event_info"]["Time"])
+        last_received = getattr(self.subject, key, None)
+        if last_received is None:
+            setattr(self.subject, key, event_time.isoformat())
+            return True
+        if parse(last_received) > event_time:
+            return False
+        setattr(self.subject, key, event_time.isoformat())
+        return True
+
+
 rulesdata = [
 
     """
-    My wonderful rule description
+    Just emit "data-received" event
+    Data should be handled by application specific logic
     """,
     {
-        rulename: "my-rule-name",  # TODO: changeme
-        subscribe_to: "my-rule-subscription",  # TODO: changeme
+        rulename: "on-data-received-propagate",
+        subscribe_to: "google.pubsub.topic.publish",
         ruledata: {
             filters: [
+                IsNotOutdated(lambda payload: "data-received#{}".format(payload.get("deviceid")))
             ],
             processing: [
+                SetSubjectPropertySilently("m_lastSeen", datetime.now().isoformat()),
+                Route("data-received",                                  # message
+                      lambda payload: payload.pop("deviceid"),          # subject
+                      lambda payload: {                                 # payload
+                          "receivedAt": payload["_event_info"]["Time"],
+                          "data": payload
+                      }),
             ],
         },
     },
