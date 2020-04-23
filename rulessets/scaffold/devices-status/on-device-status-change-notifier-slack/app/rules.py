b2217613cf9727cbea784ce85001cc8954c5581d
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-04-23 12:13:45 +0200

    implement on-device-status-change-notifier-slack

---
+++ rulessets/scaffold/devices-status/on-device-status-change-notifier-slack/app/rules.py
@@ -0,0 +1,106 @@
+import os
+
+import requests
+
+from krules_core.base_functions import *
+
+from krules_core import RuleConst as Const, messages
+from krules_core.base_functions.misc import PyCall
+
+rulename = Const.RULENAME
+subscribe_to = Const.SUBSCRIBE_TO
+ruledata = Const.RULEDATA
+filters = Const.FILTERS
+processing = Const.PROCESSING
+
+from krules_core.providers import results_rx_factory
+from krules_env import publish_results_errors, publish_results_all, publish_results_filtered
+
+# import pprint
+# results_rx_factory().subscribe(
+#     on_next=pprint.pprint
+# )
+# results_rx_factory().subscribe(
+#     on_next=publish_results_all,
+# )
+results_rx_factory().subscribe(
+    on_next=publish_results_errors,
+)
+
+rulesdata = [
+
+    """
+    Notify onboarded (READY)
+    """,
+    {
+        rulename: "on-device-ready-notify",
+        subscribe_to: messages.SUBJECT_PROPERTY_CHANGED,
+        ruledata: {
+            filters: [
+                CheckPayloadMatchOne("$.value", "READY"),
+            ],
+            processing: [
+                PyCall(
+                    requests.post,
+                    os.environ["SLACK_CHANNEL_URL"],
+                    json=lambda subject: {
+                        "type": "mrkdwn",
+                        "text": ":+1: device *{}* on board! ".format(subject.name)
+                    }
+                ),
+
+            ],
+        },
+    },
+
+    """
+    Notify ACTIVE
+    """,
+    {
+        rulename: "on-device-active-notify",
+        subscribe_to: messages.SUBJECT_PROPERTY_CHANGED,
+        ruledata: {
+            filters: [
+                CheckPayloadMatchOne("$.value",  "ACTIVE"),
+            ],
+            processing: [
+                PyCall(
+                    requests.post,
+                    os.environ["SLACK_CHANNEL_URL"],
+                    json=lambda self: {
+                        "type": "mrkdwn",
+                        "text": ":white_check_mark: device *{}* is now *{}*".format(
+                            self.subject.name, self.payload.get("value")
+                        )
+                    }
+                ),
+
+            ],
+        },
+    },
+
+    """
+    Notify INACTIVE
+    """,
+    {
+        rulename: "on-device-inactive-notify",
+        subscribe_to: messages.SUBJECT_PROPERTY_CHANGED,
+        ruledata: {
+            filters: [
+                CheckPayloadMatchOne("$.value",  "INACTIVE"),
+            ],
+            processing: [
+                PyCall(
+                    requests.post,
+                    os.environ["SLACK_CHANNEL_URL"],
+                    json=lambda self: {
+                        "text": ":ballot_box_with_check: device *{}* become *{}*".format(
+                            self.subject.name, self.payload.get("value")
+                        )
+                    }
+                ),
+            ],
+        },
+    },
+
+]
