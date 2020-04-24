5bd0f0794c7a58255203cde69660ff800654d488
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-04-24 15:20:05 +0200

    implement class-b/on-data-received-set-location

--- rulessets/apps/class-b/on-data-received-set-location/app/rules.py
+++ rulessets/apps/class-b/on-data-received-set-location/app/rules.py
@@ -2,6 +2,10 @@ from krules_core.base_functions import *
 
 from krules_core import RuleConst as Const
 
+from geopy import distance
+from geopy.geocoders import Nominatim
+
+
 rulename = Const.RULENAME
 subscribe_to = Const.SUBSCRIBE_TO
 ruledata = Const.RULEDATA
@@ -22,18 +26,37 @@ results_rx_factory().subscribe(
     on_next=publish_results_errors,
 )
 
+geolocator = Nominatim(user_agent="KRules", timeout=10)
+
+
+class SetLocationProperties(RuleFunctionBase):
+
+    def execute(self):
+
+        coords = (float(self.payload["data"]["lat"]), float(self.payload["data"]["lng"]))
+        self.subject.coords = str(coords)
+        # ensure ref point is already set
+        if "m_refCoords" not in self.subject:
+            self.subject.m_refCoords = str(coords)
+        # set location if not already set or if tolerance is exceeded
+        if "location" not in self.subject or distance.distance(
+                self.subject.m_refCoords, coords
+            ).meters > float(self.subject.tolerance):
+            self.subject.location = geolocator.reverse("{}, {}".format(coords[0], coords[1])).address
+            self.subject.m_refCoords = coords
+
+
 rulesdata = [
 
     """
-    My wonderful rule description
+    Always store coords in subject, Initialize starting point too if needed
     """,
     {
-        rulename: "my-rule-name",  # TODO: changeme
-        subscribe_to: "my-rule-subscription",  # TODO: changeme
+        rulename: "on-data-received-store-coords",
+        subscribe_to: "data-received",
         ruledata: {
-            filters: [
-            ],
             processing: [
+                SetLocationProperties()
             ],
         },
     },
