f4c56e510040d38220f69b765e8e139653cb6405
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-04-24 09:35:30 +0200

    fix import

--- rulessets/scaffold/ingestion/on-data-received-store-mongodb/app/rules.py
+++ rulessets/scaffold/ingestion/on-data-received-store-mongodb/app/rules.py
@@ -11,8 +11,8 @@ processing = Const.PROCESSING
 from krules_core.providers import results_rx_factory, settings_factory
 from krules_env import publish_results_errors, publish_results_all, publish_results_filtered
 
-from app_functions.mongodb import WithDatabase, WithCollection
-from app_functions.mongodb import set_client as set_mongodb_client
+from app_functions import mongodb as mongodb_functions  # import WithDatabase, WithCollection
+# from app_functions.mongodb import set_client as set_mongodb_client
 from pymongo import IndexModel, HASHED, MongoClient
 from dateutil.parser import parse
 
@@ -28,7 +28,7 @@ results_rx_factory().subscribe(
 )
 
 mongodb_settings = settings_factory().get("apps").get("ingestion").get("mongodb")
-set_mongodb_client(
+mongodb_functions.set_client(
     MongoClient(*mongodb_settings.get("client_args", ()), **mongodb_settings.get("client_kwargs", {}))
 )
 
@@ -42,16 +42,17 @@ rulesdata = [
         subscribe_to: "data-received",
         ruledata: {
             processing: [
-                WithDatabase(mongodb_settings["database"]),
-                WithCollection(mongodb_settings["collection"], indexes=[IndexModel([("deviceid", HASHED)])],
-                               capped=True, size=1000000,
-                               exec_func=lambda c, self:
-                                   c.insert_one({
-                                       "deviceid": self.subject.name,
-                                       "received_at": parse(self.payload["receivedAt"]),
-                                       "data": self.payload["data"],
-                                   }),
-                               ),
+                mongodb_functions.WithDatabase(mongodb_settings["database"]),
+                mongodb_functions.WithCollection(mongodb_settings["collection"],
+                                                 indexes=[IndexModel([("deviceid", HASHED)])],
+                                                 capped=True, size=1000000,
+                                                 exec_func=lambda c, self:
+                                                 c.insert_one({
+                                                     "deviceid": self.subject.name,
+                                                     "received_at": parse(self.payload["receivedAt"]),
+                                                     "data": self.payload["data"],
+                                                 }),
+                                                 ),
             ],
         },
     },
