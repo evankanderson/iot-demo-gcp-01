f9b39748f3dfdc89dcedfc40e617ded9753dba19
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-04-24 15:34:57 +0200

    implement class-b/on-location-change-notifier-postextapi

--- rulessets/apps/class-b/on-location-change-notifier-postextapi/app/rules.py
+++ rulessets/apps/class-b/on-location-change-notifier-postextapi/app/rules.py
@@ -1,6 +1,12 @@
+import os
+from datetime import datetime, timedelta
+
+import pytz
+
+from app_functions import Schedule
 from krules_core.base_functions import *
 
-from krules_core import RuleConst as Const
+from krules_core import RuleConst as Const, messages
 
 rulename = Const.RULENAME
 subscribe_to = Const.SUBSCRIBE_TO
@@ -10,32 +16,100 @@ processing = Const.PROCESSING
 
 from krules_core.providers import results_rx_factory
 from krules_env import publish_results_errors, publish_results_all, publish_results_filtered
+import requests
 
 # import pprint
 # results_rx_factory().subscribe(
 #     on_next=pprint.pprint
 # )
-# results_rx_factory().subscribe(
-#     on_next=publish_results_all,
-# )
 results_rx_factory().subscribe(
-    on_next=publish_results_errors,
+    on_next=publish_results_all,
 )
+# results_rx_factory().subscribe(
+#     on_next=publish_results_errors,
+# )
+
+
+class PostExtApi(RuleFunctionBase):
+
+    def execute(self, url, req_kwargs, on_response):
+        resp = requests.post(url, **req_kwargs)
+        self.payload["response_status"] = resp.status_code
+        self.payload["response_text"] = resp.text
+        on_response(self, resp)
+
 
 rulesdata = [
 
     """
-    My wonderful rule description
+    Dispose api call
     """,
     {
-        rulename: "my-rule-name",  # TODO: changeme
-        subscribe_to: "my-rule-subscription",  # TODO: changeme
+        rulename: "on-location-changed-notify-postextapi",
+        subscribe_to: messages.SUBJECT_PROPERTY_CHANGED,
         ruledata: {
             filters: [
+                OnSubjectPropertyChanged("location"),
             ],
             processing: [
+                Route("do-extapi-post", payload=lambda self: {
+                    "url": os.environ["EXTAPI_URL"],
+                    # https://europe-west3-krules-dev-254113.cloudfunctions.net/store_devices_location
+                    "req_kwargs": {
+                        "headers": {"x-api-key": os.environ["EXTAPI_X_API_KEY"]},
+                        "json": {
+                            "device": self.subject.name,
+                            "timestamp": datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat(),
+                            "coords": self.subject.coords,
+                            "location": self.payload["value"],
+                        }
+                    }
+                })
             ],
         },
     },
+
+    """
+    Do call api
+    """,
+    {
+        rulename: "on-do-extapi-post",
+        subscribe_to: "do-extapi-post",
+        ruledata: {
+            processing: [
+                PostExtApi(
+                    url=lambda payload: payload["url"],
+                    req_kwargs=lambda payload: payload["req_kwargs"],
+                    on_response=lambda self, resp: (
+                        resp.raise_for_status(),
+                        resp.status_code == 503 and resp.raise_for_status()
+                        ## do something when success...,
+                        # eg: Route("on-extapi-post.success", payload=resp.json())
+                    )
+                )
+            ]
+        }
+    },
+
+    """
+    Manage exception
+    """,
+    {
+        rulename: "on-do-extapi-post-errors",
+        subscribe_to: "{}-errors".format(os.environ["K_SERVICE"]),
+        ruledata: {
+            filters: [
+                IsTrue(lambda payload, jp_match1:
+                       payload.get("rule_name") == "on-do-extapi-post" and
+                       jp_match1("$.processing[*].exception", payload) == "requests.exceptions.HTTPError" and
+                       jp_match1("$.processing[*].exc_extra_info.response_code", payload) == 503)
+            ],
+            processing: [
+                Schedule(message="do-extapi-post",
+                         payload=lambda payload: payload["payload"],
+                         when=lambda _: (datetime.now() + timedelta(seconds=30)).isoformat()),
+            ]
+        }
+    }
 
 ]
